572db82a23c72f5c1240328a5bb3323d7be05a5b
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-17 19:25:06 +0300

    Initial commit

---
+++ task04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task04
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            //public static T[] GetArray<T>(this IEnumerable<T> list){...}
+            //Примените расширяющий метод к экземпляру типа MyList<T>, разработанному в домашнем задании 2 для данного урока.
+            //Выведите на экран значения элементов массива, который вернул расширяющий метод GetArray().
+        }
+    }
+}
