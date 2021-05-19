e7fae8d51373af053c3185ee5e451c9c7f848aba
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-19 17:19:32 +0300

    Task 04 completed

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using task02;
 
 namespace task04
 {
@@ -6,9 +7,24 @@ namespace task04
     {
         private static void Main()
         {
-            //public static T[] GetArray<T>(this IEnumerable<T> list){...}
-            //Примените расширяющий метод к экземпляру типа MyList<T>, разработанному в домашнем задании 2 для данного урока.
-            //Выведите на экран значения элементов массива, который вернул расширяющий метод GetArray().
+            var myList = new MyList<int>(new[] {1, 2});
+
+            foreach (var item in myList.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = { };
+            foreach (T item in list)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[^1] = item;
+            }
+
+            return array;
         }
     }
 }
